adb15cc93f3dbb66967942d24f2b14c7abb64336
Sangw00 <saraelbouchkeri@gmail>
2023-12-14 13:59:07 +0100

    14/12 am

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,49 +3,71 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Response;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
+use App\Mail\RegisterMail;
 
 class UserController extends Controller
 {
     //
 
     public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|min:8',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'username' => $request->input('username'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        // Send email verification notification
+        
+        $user->sendEmailVerificationNotification();
+
+        return response()->json(['message' => 'Email verification sent'], 200);
+    }
+
+    public function verifyEmail(Request $request)
 {
-    $validateData = $request->validate([
-        'username' => 'required|string|min:8',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|min:8',
-        'password_confirmation' => 'required|same:password',
-    ]);
+    $user = User::where('email', $request->email)->first();
 
-    $user = User::create([
-        'username' => $validateData['username'],
-        'email' => $validateData['email'],
-        'password' => Hash::make($validateData['password']),
-        'password_confirmation' => Hash::make($validateData['password']),
-    ]);
-
-    $user->sendEmailVerificationNotification();
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
+    }
 
     if ($user->hasVerifiedEmail()) {
-        $user->remember_token = $user->createToken('auth_token')->plainTextToken;
-        $user->save();
-        event(new Registered($user));
-
-        return response()->json([
-            'message' => 'User registered successfully',
-            'access_token' => $user->remember_token,
-            'data' => new UserResource($user),
-            'status' => Response::HTTP_OK
-        ]);
-    } else {
-        return response()->json(['message' => 'Registration failed'], 401);
+        return response()->json(['message' => 'Email already verified'], 200);
     }
+
+    $user->markEmailAsVerified();
+
+    // Generate a token for the user
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    event(new Registered($user));
+
+    return response()->json([
+        'message' => 'Email verified successfully',
+        'access_token' => $token,
+        'data' => new UserResource($user),
+        'status' => Response::HTTP_OK
+    ]);
 }
+
     
     public function login(Request $request){
 
